f15e40b62f1d4becbbff389786ca9148868ee652
rajasf77 <159114626+rajasf77@users.noreply.github.com>
2026-05-13 15:52:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 provider "aws" {
   region = "us-east-1" 
 }
-
+#ECR Creation
 resource "aws_ecr_repository" "my_app_warehouse" {
   name                 = "poc-app-repo"
   image_tag_mutability = "MUTABLE"
